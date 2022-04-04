5c3bc7db8a21e34c37bbe1c7e7a37f5b2ff2541e
Katie McCauley <katiemccau02@gmail.com>
2022-04-04 15:39:08 -0600

    game works

--- Program.cs
+++ Program.cs
@@ -9,71 +9,85 @@ class Program
     bool replay = true;
     String player;
     String computer;
-    while (replay)
+    String startGame;
+    Console.WriteLine("Do you want to play the game?");
+    startGame = Console.ReadLine();
+    startGame = startGame.ToUpper();
+    if (startGame == "YES")
     {
-      player = "";
-      computer = "";
-      Console.Write("Enter ROCK, PAPER OR SCISSORS!");
-      player = Console.ReadLine();
-      player = player.ToUpper();
-      if (player == "ROCK" || player == "PAPER" || player == "SCISSORS")
+
+
+      while (replay)
       {
+        player = "";
+        computer = "";
+        Console.Write("Enter ROCK, PAPER OR SCISSORS!");
+        player = Console.ReadLine();
+        player = player.ToUpper();
+        if (player == "ROCK" || player == "PAPER" || player == "SCISSORS")
+        {
 
-        var number = random.Next(1, 4);
-        if (number == 1)
+          var number = random.Next(1, 4);
+          if (number == 1)
+          {
+            computer = "ROCK";
+          }
+          else if (number == 2)
+          {
+            computer = "PAPER";
+          }
+          else if (number == 3)
+          {
+            computer = "SCISSORS";
+          }
+
+          Console.WriteLine("Player: " + player);
+          Console.WriteLine("Computer: " + computer);
+
+          if (player == "ROCK" && computer == "ROCK")
+          {
+            Console.WriteLine("It is a draw!");
+          }
+          if (player == "ROCK" && computer == "PAPER")
+          {
+            Console.WriteLine("paper wins");
+          }
+          if (player == "ROCK" && computer == "SCISSORS")
+          {
+            Console.WriteLine("rock wins");
+          }
+          if (player == "SCISSORS" && computer == "SCISSORS")
+          {
+            Console.WriteLine("Its a tie");
+          }
+          if (player == "SCISSORS" && computer == "PAPER")
+          {
+            Console.WriteLine("Scissors wins the game");
+          }
+          if (player == "SCISSORS" && computer == "ROCK")
+          {
+            Console.WriteLine("rocks beats scisors");
+          }
+          if (player == "PAPER" && computer == "PAPER")
+          {
+            Console.WriteLine("it is a tie");
+          }
+          if (player == "PAPER" && computer == "ROCK")
+          {
+            Console.WriteLine("paper wins the gamee");
+          }
+          if (player == "PAPER" && computer == "SCISSORS")
+          {
+            Console.WriteLine("scirrosies wins the game");
+          }
+        };
+        if (player == "QUIT")
         {
-          computer = "ROCK";
-        }
-        else if (number == 2)
-        {
-          computer = "PAPER";
-        }
-        else if (number == 3)
-        {
-          computer = "SCISSORS";
+          Console.WriteLine("the game is over");
+          return;
         }
 
-        Console.WriteLine("Player: " + player);
-        Console.WriteLine("Computer: " + computer);
-
-        if (player == "ROCK" && computer == "ROCK")
-        {
-          Console.WriteLine("It is a draw!");
-        }
-        if (player == "ROCK" && computer == "PAPER")
-        {
-          Console.WriteLine("paper wins");
-        }
-        if (player == "ROCK" && computer == "SCISSORS")
-        {
-          Console.WriteLine("rock wins");
-        }
-        if (player == "SCISSORS" && computer == "SCISSORS")
-        {
-          Console.WriteLine("Its a tie");
-        }
-        if (player == "SCISSORS" && computer == "PAPER")
-        {
-          Console.WriteLine("Scissors wins the game");
-        }
-        if (player == "SCISSORS" && computer == "ROCK")
-        {
-          Console.WriteLine("rocks beats scisors");
-        }
-        if (player == "PAPER" && computer == "PAPER")
-        {
-          Console.WriteLine("it is a tie");
-        }
-        if (player == "PAPER" && computer == "ROCK")
-        {
-          Console.WriteLine("paper wins the gamee");
-        }
-        if (player == "PAPER" && computer == "SCISSORS")
-        {
-          Console.WriteLine("scirrosies wins the game");
-        }
       }
-
     }
 
     Console.ReadKey();
